c2bebc45cfc2f92ea509d91a7a9fba0f293a5df5
daewoongBang <daewoong.bang@gmail.com>
2020-01-22 09:59:48 +0900

    Updates

---
+++ precache-manifest.710fc687653498cbcc510feb0ab37eaa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "058e132e2a62c33697149e4e966f68f3",
+    "url": "/mashup-todolist/index.html"
+  },
+  {
+    "revision": "a06b17dbb924002f15ea",
+    "url": "/mashup-todolist/static/js/2.27902082.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/mashup-todolist/static/js/2.27902082.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8169f9bf449e8ffdb4e6",
+    "url": "/mashup-todolist/static/js/main.2837e9f9.chunk.js"
+  },
+  {
+    "revision": "d00e09345e78e8231429",
+    "url": "/mashup-todolist/static/js/runtime-main.8f64f1cb.js"
+  }
+]);
